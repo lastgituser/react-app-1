0b55cebe2691197357d7a040e62d3827c1808a98
lastgituser <31521320+lastgituser@users.noreply.github.com>
2022-03-16 00:45:35 -0400

    Changed home text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn with me
         </a>
       </header>
     </div>
